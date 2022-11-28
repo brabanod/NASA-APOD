fb03adff789409ed9abead75548a16b15b61b787
Pascal Braband <pascal.braband@web.de>
2022-11-28 08:08:31 +0100

    Refactored APODCache, created async method for accessing an APOD from cache.

--- NASA-APOD/Model/APODCache.swift
+++ NASA-APOD/Model/APODCache.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Combine
 
-// FIXME: Shoudl this be an actor?
+// FIXME: Should this be an actor?
 class APODCache {
     
     // MARK: Model
@@ -18,10 +18,9 @@ class APODCache {
     /// Specifies how many APODs are loaded, when cache is initialized.
     private(set) var initialLoadAmount: Int
     
-    /// Stores loaded APODs
+    /// Stores loaded APODs (cache)
     private var apods: [Date: APOD] = [:]
     
-    private var initalLoadTask: Task<[APOD], Error>? = nil
     private var apodTasks: [Date: Task<APOD, Error>] = [:]
     private var thumbnailTasks: [Date: Task<APOD, Error>] = [:]
     private var imageTasks: [Date: Task<APOD, Error>] = [:]
@@ -29,6 +28,11 @@ class APODCache {
     
     // MARK: -
     
+    /// - Parameters:
+    ///     - apodAPI: The API instance to use when loading APODs.
+    ///     - initialLoadAmout: The amount of APODs that should be loaded into cache directly when the cache is initialized.
+    ///     - withThumbnails: Specifies, whether the initially loaded APODs should be loaded with thumbnails.
+    ///     - withImages: Specifies, whether the initially loaded APODs should be loaded with images.
     init(
         api apodAPI: APODAPI,
         initialLoadAmount: Int = 10,
@@ -43,51 +47,27 @@ class APODCache {
     
     func initialLoad(withThumbnails shouldLoadThumbnails: Bool, withImages shouldLoadImages: Bool) async throws {
         // Store retrieved APODs at their individual position in the cache
-        for days in (1...self.initialLoadAmount) {
+        for days in (0..<self.initialLoadAmount) {
             // Get date, continue to next if this fails
-            guard let date = DateUtils.today(adding: -days)
+            guard let date = DateUtils.today(adding: -(days+1))
             else {
                 // Try next APOD
                 continue
             }
             
-            // Load APOD with a continuation, so that it can await for all completion handlers to finish
-            try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
-                // Keep track if all completition handlers are finished. Don' wait for thumbnails and images if not requested
-                // Continuation should only be resumed if alle are true
-                var isMetadataLoaded = false
-                var isThumbnailLoaded = false || !shouldLoadThumbnails
-                var isImageLoaded = false || !shouldLoadImages
-                
-                do {
-                    try apod(for: date,
-                             completion: { _ in
-                        isMetadataLoaded = true
-                        if isMetadataLoaded, isThumbnailLoaded, isImageLoaded {
-                            continuation.resume()
-                        }
-                    },
-                             withThumbnail: shouldLoadThumbnails,
-                             thumbnailCompletion: { _ in
-                        isThumbnailLoaded = true
-                        if isMetadataLoaded, isThumbnailLoaded, isImageLoaded {
-                            continuation.resume()
-                        }
-                    },
-                             withImage: shouldLoadImages,
-                             imageCompletion: { _ in
-                        isImageLoaded = true
-                        if isMetadataLoaded, isThumbnailLoaded, isImageLoaded {
-                            continuation.resume()
-                        }
-                    })
-                } catch {
-                    continuation.resume(with: .failure(error))
-                }
-            }
+            try await apod(for: date, withThumbnail: shouldLoadThumbnails, withImage: shouldLoadImages)
         }
     }
     
+    /// Loads an APOD either from cache or from API.
+    ///
+    /// - Parameters:
+    ///     - date: The date for which to load the APOD.
+    ///     - completion: This function is called, when loading metadata of APOD is completed.
+    ///     - shouldLoadThumbnail: Specifies, if thumbnail should be loaded for APOD.
+    ///     - thumbnailCompletion: This function is called, when loading the thumbnail of APOD is completed.
+    ///     - shouldLoadImage: Specifies, if image should be loaded for APOD.
+    ///     - imageCompletion: This function is called when loading the image of APOD is completed.
     func apod(
         for date: Date,
         completion: @escaping (APOD) -> (),
@@ -167,7 +147,57 @@ class APODCache {
             }
         }
     }
+    
+    /// Convenience method for retrieving APOD asynchronously. This method allows to use await, but instead does not offer the functionality to be notified on individual loading completions (metadata, thumbnail, image)
+    ///
+    /// - Parameters:
+    ///     - date: The date for which to load the APOD.
+    ///     - shouldLoadThumbnail: Specifies, if thumbnail should be loaded for APOD.
+    ///     - shouldLoadImage: Specifies, if image should be loaded for APOD.
+    ///
+    /// - Returns: The requested APOD with thumbnail and image already loaded if requested.
+    func apod(
+        for date: Date,
+        withThumbnail shouldLoadThumbnail: Bool = false,
+        withImage shouldLoadImage: Bool = false)
+    async throws -> APOD {
+        // Load APOD with a continuation, so that it can await for all completion handlers to finish
+        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<APOD, Error>) in
+            // Keep track if all completition handlers are finished. Don' wait for thumbnails and images if not requested
+            // Continuation should only be resumed if alle are true
+            var isMetadataLoaded = false
+            var isThumbnailLoaded = false || !shouldLoadThumbnail
+            var isImageLoaded = false || !shouldLoadImage
+            
+            do {
+                try apod(for: date,
+                         completion: { apod in
+                    isMetadataLoaded = true
+                    if isMetadataLoaded, isThumbnailLoaded, isImageLoaded {
+                        continuation.resume(with: .success(apod))
+                    }
+                },
+                         withThumbnail: shouldLoadThumbnail,
+                         thumbnailCompletion: { apod in
+                    isThumbnailLoaded = true
+                    if isMetadataLoaded, isThumbnailLoaded, isImageLoaded {
+                        continuation.resume(with: .success(apod))
+                    }
+                },
+                         withImage: shouldLoadImage,
+                         imageCompletion: { apod in
+                    isImageLoaded = true
+                    if isMetadataLoaded, isThumbnailLoaded, isImageLoaded {
+                        continuation.resume(with: .success(apod))
+                    }
+                })
+            } catch {
+                continuation.resume(with: .failure(error))
+            }
+        }
+    }
 }
 
-// FIXME: All tasks need to be removed after accessed
 // FIXME: Make APOD an actor because the APOD object is accessed asynchronously by thumbnail AND image task. This makes it vulnerable to race conditions. Therefore create a method which allows us to setThumbnail and setImage for the APOD.
+// FIXME: Should not create a copy and then store it again, this is extremly dangerous. Instead mutate the object, only using setters.
+// TODO: Find a way so that it is not possible to give completion handler for thumbnails or images but not loading any of these. This would reult in the inital loading to wait for ever to complete.
